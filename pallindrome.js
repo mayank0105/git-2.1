a3f7d7ab79bc20aae5790076870f6b27a6142d8e
SHRESHTH YT <shreshthrajvijaykumarsah@outlook.com>
2023-01-10 18:25:05 +0530

    fixed the palindrome code

--- pallindrome.js
+++ pallindrome.js
@@ -1,12 +1,27 @@
-let str="naman";
-let bag="";
+function checkpalindrome(str){
+	let bag="";
 
-for(let i=str.length-1; i>=1; i++)
-{
-	bag+=str[i];
+	for(let i=str.length-1; i>=0; i--)
+	{
+		bag+=str[i];
+	}
+	
+		console.log(bag)
+	
+	if(str===bag){
+		console.log(`${str} is Palindrome`)
+	}
+	else{
+		console.log(`${str} is not Palindrome`)
+	}
+	
+	
 }
 
-if(	
+checkpalindrome('naman')
+checkpalindrome('shreshth')
+checkpalindrome('mayank')
 
 
-//please check this code shreshth
+//please check this code shreshth 
+//done....
